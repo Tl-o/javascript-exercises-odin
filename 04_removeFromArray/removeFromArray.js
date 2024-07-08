ec2cf59f2bee2bbcd786f119313824cb6add6888
Tl-o <113454837+Tl-o@users.noreply.github.com>
2024-07-08 23:57:10 +0300

    Finish 04-removeFromArray using filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toDelete) {
+  array = array.filter((el) => !toDelete.includes(el));
+  return array;
 };
 
 // Do not edit below this line
