acf0908b3313ef7c424ff5fdc5e26722822e373f
Tl-o <113454837+Tl-o@users.noreply.github.com>
2024-07-09 00:25:37 +0300

    Finish 07-tempConversion, while sleep-deprived

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fehrenheit) {
+  const celsius = (fehrenheit - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const fehrenheit = celsius * (9 / 5) + 32;
+  return Number(fehrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
