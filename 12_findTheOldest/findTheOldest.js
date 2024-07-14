fd50a4887315693e5b9f422a4e5cbc47156da4cf
Tl-o <113454837+Tl-o@users.noreply.github.com>
2024-07-14 07:15:54 +0300

    Finish all

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currPerson) => {
+    let currAge = getAge(currPerson);
+    let oldestAge = getAge(oldestPerson);
 
+    return currAge > oldestAge ? currPerson : oldestPerson;
+  });
+};
+
+const getAge = function (person) {
+  return person.yearOfDeath
+    ? person.yearOfDeath - person.yearOfBirth
+    : new Date().getFullYear() - person.yearOfBirth;
 };
 
 // Do not edit below this line
