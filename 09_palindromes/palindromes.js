dc5b82a4f573b1e5ad5bd402fad68ce9ff995ef2
Tl-o <113454837+Tl-o@users.noreply.github.com>
2024-07-14 06:32:22 +0300

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Removes all punctuation and white spaces.
+  const punctuationless = string
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replaceAll(" ", "")
+    .toLowerCase();
 
+  const stringLength = punctuationless.length;
+  for (let i = 0; i < stringLength; i++) {
+    if (punctuationless[i] !== punctuationless[stringLength - 1 - i])
+      return false;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
