2314297a9d5301ec0a2c0b07c0fbc21339d4af16
Tl-o <113454837+Tl-o@users.noreply.github.com>
2024-07-14 06:59:27 +0300

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (until) {
+  until = +until;
+  if (until < 0) return "OOPS"; // Does not accept negatives.
 
+  let firstPrev = 1,
+    secondPrev = 0;
+
+  if (until === 0) return until;
+
+  for (let i = 2; i <= until; i++) {
+    let count = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = count;
+  }
+
+  return firstPrev;
 };
 
 // Do not edit below this line
